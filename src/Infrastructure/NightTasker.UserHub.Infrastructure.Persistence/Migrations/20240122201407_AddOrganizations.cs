3503605324a38cd14878d525f69e3241ac1ad5a3
Danila Uprivanov <mr.uprivanov@yandex.ru>
2024-01-22 23:59:46 +0300

    add organizations migration

---
+++ src/Infrastructure/NightTasker.UserHub.Infrastructure.Persistence/Migrations/20240122201407_AddOrganizations.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NightTasker.UserHub.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrganizations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "organization_id",
+                table: "user_infos",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "organization",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(254)", maxLength: 254, nullable: true),
+                    created_date_time_offset = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    updated_date_time_offset = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_organization", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "organization_user",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    organization_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_organization_user", x => new { x.organization_id, x.user_id });
+                    table.ForeignKey(
+                        name: "fk_organization_user_organization_organization_id",
+                        column: x => x.organization_id,
+                        principalTable: "organization",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_organization_user_user_info_user_info_id",
+                        column: x => x.user_id,
+                        principalTable: "user_infos",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_infos_organization_id",
+                table: "user_infos",
+                column: "organization_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_organization_user_user_id",
+                table: "organization_user",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_user_infos_organization_organization_id",
+                table: "user_infos",
+                column: "organization_id",
+                principalTable: "organization",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_user_infos_organization_organization_id",
+                table: "user_infos");
+
+            migrationBuilder.DropTable(
+                name: "organization_user");
+
+            migrationBuilder.DropTable(
+                name: "organization");
+
+            migrationBuilder.DropIndex(
+                name: "ix_user_infos_organization_id",
+                table: "user_infos");
+
+            migrationBuilder.DropColumn(
+                name: "organization_id",
+                table: "user_infos");
+        }
+    }
+}
